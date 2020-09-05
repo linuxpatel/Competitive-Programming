2b030112c44e6969b9baf6c7ffc9053a1620ed97
Shreya <dhirshreya@gmail.com>
2020-09-06 00:02:11 +0530

    Update

--- HackerEarth/MonkandRotation.c
+++ HackerEarth/MonkandRotation.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-
-int main(){
-	int N, T, K;
-	int A[0];
-	scanf("%d", &T);
-	scanf("%d", &N);
-	scanf("%d", &K);
-	printf("Input number is %d.\n", N);
-	for(int i=0; i<N ; i++)
-	{
-		scanf("%d ",A[i]);
-	}            			// Reading input from STDIN
-	     // Writing output to STDOUT
-for(int i=0; i<K; i++)
-	{
-		for(int j=K; i< N-1; i++)
-		{
-			for(int k=i; k< N-1; k--)
-			{
-				printf("%d ",&A[k]);
-			}
-		}
-	}
-	return 0;
-}
+// #include <stdio.h>
+//
+// int main(){
+// 	int N, T, K;
+// 	int A[0];
+// 	scanf("%d", &T);
+// 	scanf("%d", &N);
+// 	scanf("%d", &K);
+// 	printf("Input number is %d.\n", N);
+// 	for(int i=0; i<N ; i++)
+// 	{
+// 		scanf("%d ",A[i]);
+// 	}            			// Reading input from STDIN
+// 	     // Writing output to STDOUT
+// for(int i=0; i<K; i++)
+// 	{
+// 		for(int j=K; i< N-1; i++)
+// 		{
+// 			for(int k=i; k< N-1; k--)
+// 			{
+// 				printf("%d ",&A[k]);
+// 			}
+// 		}
+// 	}
+// 	return 0;
+// }
