9ccfe1b9dd7e7a4cb3a90afe6046b925198e0c35
ShreyaDhir <dhirshreya@gmail.com>
2020-10-08 03:49:30 +0530

    Update

--- LeetCode/Easy/Palindrome.cpp
+++ LeetCode/Easy/Palindrome.cpp
@@ -4,15 +4,18 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(unsigned int x) {
-      // if(x<0)
-      //  {x=-x;}
         unsigned int res=0;
-         while(true)
-         {
-             res=res*10+x%10;
-             x=x/10;
-         }
-    return res;
+        if(x<0 || (x%10==0 && x!=0))
+            return 0;
+            
+          while(x>res || (x%10==0 && x!=0))
+          {
+              res=res*10+x%10;
+           x=x/10;
+
+          }
+          return (x==res) || (x==res/10);
+          cout<<res<<endl;
     }
 };
 
